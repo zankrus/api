f24e8d8fff7eee34a28e8b168baf13bb4f1be2a9
Ro <sealthepirate@gmail.com>
2020-08-07 21:29:26 +0300

    message

--- tests/test_booking.py
+++ tests/test_booking.py
@@ -5,3 +5,11 @@ def test_create_booking(auth_client):
     booking_data = BookingData().random()
     res = auth_client.create_booking(booking_data)
     assert res.status_code == 200
+    assert "bookingid" in str(res.content)
+
+
+def test_negative_booking(unauth_client):
+    booking_data = BookingData(firstname=None)
+    res = unauth_client.create_booking(booking_data)
+    assert res.status_code == 500
+    assert res.content == b'Internal Server Error'
